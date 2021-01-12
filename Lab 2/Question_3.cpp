8cc2cffbf524b0a5a22371c4b9e33b292b79e466
Abhijeet Mahapatra <abhi10012001@gmail.com>
2021-01-12 20:05:07 +0530

    Question 3

---
+++ Lab 2/Question_3.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class triangle
+{
+    public:
+        void upper_left_triangle(int n);
+        void upper_right_triangle(int n);
+        void lower_left_triangle(int n);
+        void lower_right_triangle(int n);
+        void hourglass(int n);
+};
+
+void triangle::upper_left_triangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= n-i; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void triangle::upper_right_triangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = i+1; j <= n; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void triangle::lower_left_triangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void triangle::lower_right_triangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n-i; j >= 1; j--)
+        {
+            cout<<"  ";
+        }
+        for (int j = 1; j <= i ; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void triangle::hourglass(int n)
+{
+    //Upper triangle
+    for (int i = 0; i < ceil(n/2); i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 1+i; j <= n-i; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    //Lower triangle
+    for (int i = ceil(n/2)-1; i >= 0; i--)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout<<"  ";
+        }
+        for (int j = i+1; j <= n-i; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    triangle create;
+    cout<<"\nUpper Left Triangle:\n";
+    create.upper_left_triangle(n);
+    cout<<"\nUpper Right Triangle:\n";
+    create.upper_right_triangle(n);
+    cout<<"\nLower Left Triangle:\n";
+    create.lower_left_triangle(n);
+    cout<<"\nLower Left Triangle:\n";
+    create.lower_right_triangle(n);
+    cout<<"\nHourglass:\n";
+    create.hourglass(n);
+    return 0;
+}
